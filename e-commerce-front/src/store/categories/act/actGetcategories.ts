c25e97401c0e0950113da42a4a1ea01496acfb73
hemade <hemade@gmail.com>
2025-03-23 13:09:53 +0200

    #

--- e-commerce-front/src/store/categories/act/actGetcategories.ts
+++ e-commerce-front/src/store/categories/act/actGetcategories.ts
@@ -3,6 +3,9 @@ import { TCategory } from "@type";
 import axiosErrorHandler from "@utils/axiosErrorHandler";
 import axios from "axios";
 
+// axios.defaults.baseURL = "https://api.example.com";
+axios.defaults.baseURL = "/api"; // يجب أن يكون في ملف إعداد مثل src/axiosConfig.js
+
 type TRespons = TCategory[];
 
 const actGetcategories = createAsyncThunk(
