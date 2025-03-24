7650449a5af1a8ed22d905e9749351311340cc3a
hemade <hemade@gmail.com>
2025-03-24 13:29:10 +0200

    #

--- e-commerce-back/index.js
+++ e-commerce-back/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./utils/db");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const productRoutes = require("./routes/productRoutes");
@@ -10,15 +11,16 @@ const app = express();
 // الاتصال بـ MongoDB
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 
 // تعيين المسارات
 app.use("/users", userRoutes);
-app.use("api/categories", categoryRoutes);
+app.use("/categories", categoryRoutes);
 app.use("/products", productRoutes);
 app.use("/wishlist", wishlistRoutes);
 
 // للاختبار المحلي
-// app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => console.log("Server running on port 3000"));
 
 module.exports = app;
