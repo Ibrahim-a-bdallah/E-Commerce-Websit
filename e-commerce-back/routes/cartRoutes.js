32ae9ef38dc90fa2054cff65f0095fad9bb1fdfa
hemade <hemade@gmail.com>
2025-03-27 12:55:09 +0200

    #

--- e-commerce-back/routes/cartRoutes.js
+++ e-commerce-back/routes/cartRoutes.js
@@ -5,15 +5,11 @@ const Product = require("../models/product");
 router.get("/", async (req, res) => {
   try {
     const { prifixIds } = req.query; // استخراج prifixIds من الاستعلام
-    let products;
-
-    if (cat_prefix) {
-      // جلب المنتجات التي تطابق prifixIds
-      products = await Product.find({ prifixIds });
-    } else {
-      // جلب جميع المنتجات إذا لم يتم تحديد prifixIds
-      products = await Product.find();
-    }
+    const products = await Product.find({
+      prefixIds: {
+        $in: prifixIds.split(","),
+      },
+    });
 
     res.json(products);
   } catch (error) {
