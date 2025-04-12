aaf20420d4b5fd9727d99aea261e946829e8904f
hemade <hemade@gmail.com>
2025-04-12 22:55:45 +0200

    update aboutUs

--- e-commerce-front/src/pages/AboutUs.tsx
+++ e-commerce-front/src/pages/AboutUs.tsx
@@ -1,5 +1,25 @@
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p style={{ opacity: ".6", lineHeight: "1.8", padding: "20px" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, quasi?
+        Saepe mollitia quisquam placeat cumque beatae repellat quibusdam
+        consequuntur recusandae laudantium perspiciatis? Iusto inventore
+        asperiores exercitationem laudantium officiis, amet saepe! Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. Illum beatae alias
+        officiis tenetur quidem earum error? Recusandae rerum voluptatibus aut
+        soluta excepturi adipisci aliquid vitae, facere ratione, ad et
+        repudiandae? Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Expedita amet nostrum voluptatem voluptatum, quas, libero voluptate
+        vitae totam, delectus illum consequuntur dolore placeat? Alias voluptas
+        laudantium esse, porro deleniti provident? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Et unde at veniam omnis sapiente eos
+        repellendus dolor ipsum distinctio delectus excepturi laboriosam
+        assumenda, neque vitae non nesciunt, error nobis iste.
+      </p>
+    </div>
+  );
 };
 
 export default AboutUs;
