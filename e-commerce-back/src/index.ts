8efa1eebcd3250c42bf2cb6300f214cba350d8a1
hemade <hemade@gmail.com>
2025-05-19 14:56:09 +0300

    #

--- e-commerce-back/src/index.ts
+++ e-commerce-back/src/index.ts
@@ -15,7 +15,12 @@ const app = express();
 // الاتصال بـ MongoDB
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/", router);
 
