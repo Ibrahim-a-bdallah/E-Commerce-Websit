96c3a21261a4338bed2fb31f1e664a6c18527493
hemade <hemade@gmail.com>
2025-05-19 17:52:40 +0300

    #

--- e-commerce-back/src/index.ts
+++ e-commerce-back/src/index.ts
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
-import connectDB from "./src/utils/db";
+import connectDB from "./utils/db";
 import cors from "cors";
-import authController from "./src/controllers/sign.Controller";
-import categoryRoutes from "./src/routes/categoryRoutes";
-import productRoutes from "./src/routes/productRoutes";
-import wishlistRoutes from "./src/routes/wishlistRoutes";
-import router from "./src/routes";
+import authController from "./controllers/sign.Controller";
+import categoryRoutes from "./routes/categoryRoutes";
+import productRoutes from "./routes/productRoutes";
+import wishlistRoutes from "./routes/wishlistRoutes";
+import router from "./routes";
 
 dotenv.config();
 
